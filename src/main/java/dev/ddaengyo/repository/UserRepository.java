a88d16837f0393a6f71b1df91b4fa3d2233759a2
jsssun <gmhie1208@gmail.com>
2026-03-17 16:43:09 +0900

    feat: html

--- src/main/java/dev/ddaengyo/repository/UserRepository.java
+++ src/main/java/dev/ddaengyo/repository/UserRepository.java
@@ -2,8 +2,12 @@ package dev.ddaengyo.repository;
 
 import dev.ddaengyo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
